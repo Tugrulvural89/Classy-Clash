675da10b7db44556afa52925886857dc27ff5cbf
tugrulvural89 <tugrulv89@gmail.com>
2024-02-10 07:00:45 -0800

    update Char Class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,72 @@
 #include "raylib.h"
 #include "raymath.h"
+
+class Character
+{
+private:
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Texture2D texture{LoadTexture("C:/Users/Shadow/CLionProjects/RaylibProject2/characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("C:/Users/Shadow/CLionProjects/RaylibProject2/characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("C:/Users/Shadow/CLionProjects/RaylibProject2/characters/knight_run_spritesheet.png")};
+    // 1 right -1 left
+    float rightLeft{1.0f};
+
+    // animation character
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.0f / 12.0f};
+
+    const float speed{4.0f};
+public:
+    Vector2 getWorldPos(){return worldPos;}
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+};
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width/6),
+        (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height),
+    };
+
+}
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if(IsKeyDown(KEY_A))direction.x -= 1.0;
+    if(IsKeyDown(KEY_D))direction.x += 1.0;
+    if(IsKeyDown(KEY_W))direction.y -= 1.0;
+    if(IsKeyDown(KEY_S))direction.y += 1.0;
+    if(Vector2Length(direction)!= 0.0)
+    {
+        // set mappos
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x <= 0.f ? rightLeft = -1.0f : rightLeft = 1.0f;
+        texture = run;
+
+    } else
+    {
+        texture = idle;
+    }
+
+    runningTime += deltaTime;
+    if(runningTime >= updateTime)
+    {
+        runningTime = 0.0f;
+        frame++;
+        if(frame >= maxFrames)frame = 0;
+    }
+    Rectangle source{frame * (float)texture.width/6, 0.f, rightLeft * (float)texture.width/6.0f, (float)texture.height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width/6.0f, 4.0f * (float)texture.height};
+    Vector2 origin{};
+    float rotation{};
+    DrawTexturePro(texture, source, dest , origin, rotation, WHITE);
+
+}
 int main()
 {
     const int windowWidth{2000};
@@ -10,34 +77,20 @@ int main()
     Vector2 mapPos{0, 0};
     SetTargetFPS(60);
 
-    Texture2D knight = LoadTexture("C:/Users/Shadow/CLionProjects/RaylibProject2/characters/knight_idle_spritesheet.png");
-    Vector2 knightPos{
-            (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)knight.width/6),
-        (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)
-    };
-    float speed{4};
+
+    Character knight;
+    knight.setScreenPos(windowWidth, windowHeight);
+
+
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
-        Vector2 direction{};
-        if(IsKeyDown(KEY_A))direction.x -= 1.0;
-        if(IsKeyDown(KEY_D))direction.x += 1.0;
-        if(IsKeyDown(KEY_W))direction.y -= 1.0;
-        if(IsKeyDown(KEY_S))direction.y += 1.0;
-        if(Vector2Length(direction)!= 0.0)
-        {
-            // set mappos
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-        }
         DrawTextureEx(map, mapPos, 0.0, 5, WHITE);
-        Rectangle source{0.f, 0.f, (float)knight.width/6.0f, (float)knight.height};
-        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-        Vector2 origin{};
-        float rotation{};
-        DrawTexturePro(knight, source, dest , origin, rotation, WHITE);
+        knight.tick(GetFrameTime());
         EndDrawing();
     }
     CloseWindow();
